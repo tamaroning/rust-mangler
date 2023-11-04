3058f2a3102214e435d8dfd78776c335a868fd85
Raiki Tamura <tamaron1203@gmail.com>
2023-11-04 14:37:14 +0900

    ok

--- a.rs
+++ a.rs
@@ -1,8 +1,19 @@
-pub fn main() {
-    let f = |a: i32| a + 1;
-    let _ = f(1);
-}
+struct S();
 
 pub mod mod_a {
     pub fn f() {}
+}
+
+pub fn generic_func<T>(t: T) -> T {
+    t
+}
+
+pub fn main() {
+    mod_a::f();
+
+    let f = |a: i32| a + 1;
+    let _ = f(1);
+
+    generic_func::<i32>(0);
+    generic_func::<S>(S());
 }
